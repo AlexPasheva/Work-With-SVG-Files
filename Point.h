39ebe3e6b87e4996e12ee70db34df3869b626ffc
AlexPasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-05-05 13:42:27 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -6,24 +6,41 @@ using namespace std;
 class Point
 {
 private:
-	double x;
-	double y;
+    double x;
+    double y;
 public:
+    Point(double x = 0, double y = 0);
+
     double GetX();
     double GetY();
+    void SetX(double x);
+    void SetY(double y);
 
     // Distance to another point.  Pythagorean thm.
     double Dist(Point other);
     // Print the point on the stream.
     void Print(ostream& strm);
 };
-double Point::GetX() 
-{ 
+Point::Point(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+}
+double Point::GetX()
+{
     return x;
 }
-double Point::GetY() 
+double Point::GetY()
 {
     return y;
+}
+void Point::SetX(double x)
+{
+    this->x = x;
+}
+void Point::SetY(double y)
+{
+    this->y = y;
 }
 double Point::Dist(Point other)
 {
