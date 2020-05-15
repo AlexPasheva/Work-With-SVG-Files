640705e6f7554d6b78b3d766f26fd73680b4acab
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-05-15 15:05:08 +0300

    Update Commands.h

--- Commands.h
+++ Commands.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Shapes.h"
+#include "ShapesContainer.h"
 
 void Help()
 {
@@ -16,82 +16,42 @@ void Help()
 		"translate [<n>]  transaltes the nth shape or if n is not given, translates all shapes" << endl <<
 		"whitin <option>  prints all shapes in that region" << endl ;
 }
-void Print(Shapes* array, int ArraySize)
+void Print(ShapesContainer array)
 {
-	for (int i = 0; i < ArraySize; i++)
-	{
-		array[i].Print(cout);
-	}
+	array.PrintAll();
 }
-Shapes* CreateShapes(Shapes* array, int ArraySize, char* shape)
+Shapes* CreateShapes(ShapesContainer array, char* shape, double startX, double startY, const char* color, double endX, double endY = 0)
 {
-	Shapes* NewArray = new Shapes[ArraySize + 1];
-	if (strcmp(shape,"line")==0)
-	{
-		double startY, startX, endX, endY;
-		cin >> startY >> startX >> endX >> endY;
-		char color[25];
-		cin.getline(color, 25);
-		int ID;
-		cin >> ID;
-		Line line;
-		NewArray= line.Create(array, ArraySize, startX, startY, endX, endY, color, ID);
-	}
-	else if (strcmp(shape, "circle") == 0)
-	{
-		double startY, startX, radius;
-		cin >> startY >> startX >> radius;
-		char color[25];
-		cin.getline(color, 25);
-		int ID;
-		cin >> ID;
-		Circle circle;
-		NewArray = circle.Create(array, ArraySize, startX, startY, radius, color, ID);
-	}
-	else if (strcmp(shape, "rectangle") == 0)
-	{
-		double startY, startX, width, height;
-		cin >> startY >> startX >> width >> height;
-		char color[25];
-		cin.getline(color, 25);
-		int ID;
-		cin >> ID;
-		Rectangle rectangle;
-		NewArray = rectangle.Create(array, ArraySize, startX, startY, width , height, color, ID);
-	}
-	else
-	{
-		cout << "We can't create that shape at the moment.";
-		return;
-	}
-	return NewArray;
+	array.AddShape(shape, startX, startY, color, endX, endY);
 }
-//Shapes* shapeCollection[10] ;
-//shapeCollection[0] = new Circle(parameters);
-void TranslateShape(Shapes* array, int ArraySize, double vertical, double horizontal, int n = -1)//this function works with array like the one above
-{                                                                                                // it may work better with std container tho
+void TranslateShape(ShapesContainer& array, double vertical, double horizontal, int n = -1)
+{                                                                                                
 	if (n == -1)
 	{
-		for (int i = 0; i < ArraySize; i++)
+		for (int i = 0; i < array.GetCapacity(); i++)
 		{
-			array[i].Translate(vertical, horizontal);
+			array.AtIndex(i)->Translate(vertical,horizontal);
 		}
 	}
 	else
 	{
-		array[n].Translate(vertical, horizontal);
+		array.AtIndex(n)->Translate(vertical, horizontal);
 	}
 }
-void WithinShape(Shapes* array, int ArraySize, double startX, double startY, double width, double height = 0)
+void WithinShape(ShapesContainer& array, double startX, double startY, double width, double height = 0)
 {
 	if (height == 0)
 	{
-		for (int i = 0; i < ArraySize; i++)
-			array[i].WithinCircle(startX, startY, width);
+		for (int i = 0; i < array.GetCapacity(); i++)
+			array.AtIndex(i)->WithinCircle(startX, startY, width);
 	}
 	else
 	{
-		for (int i = 0; i < ArraySize; i++)
-			array[i].WhitinRectangle(startX, startY, width, height);
+		for (int i = 0; i < array.GetCapacity(); i++)
+			array.AtIndex(i)->WhitinRectangle(startX, startY, width, height);
 	}
+}
+void EraseShape(ShapesContainer& array, int index)
+{
+	array.Erase(index);
 }
