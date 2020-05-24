d01c374ce815d32cd0ab3ceee8a8e7a2cfbfc852
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-05-24 21:50:05 +0300

    Update WorkWithSVGFiles.cpp

--- WorkWithSVGFiles.cpp
+++ WorkWithSVGFiles.cpp
@@ -1,187 +1,125 @@
-#pragma once
-#include "ShapesContainer.h"
-#include <string>
-void Open(string filename)
-{
-	ifstream input;
-	input.open(filename);
-	if (!input.is_open())
-	{
-		cout << "Couldn't open the file.";
-	}
-}
-int LinesCount(string filename)
-{
-	ifstream input;
-	input.open(filename);
-	string line;
-	unsigned int curLine = 0;
-	while (getline(input, line))
-	{
-		curLine++;
-	}
-	input.close();
-	return curLine;
-}
-ShapesContainer ParseFile(string filename)
+#include <iostream>
+#include "SVGParser.h"
+int main()
 {
 	ShapesContainer array;
-	int currsor=LinesCount(filename);//the total lines in the file
 	ifstream input;
-	string line;                     // I'm using that string to get rid of chars I do not need
-	string token = "<svg>";
-	input.open(filename);
-	unsigned int curLine = 0;
-	while (getline(input, line))     // curLine is the index of the start of the <svg> tag
-	{
-		curLine++;
-		if (line.find(token, 0) != string::npos) 
-		{
-			break;
-		}
-	}                                // I'm finding curLine by searching for the <svg> tag
 
-	string shape;
-	int LinesLeft = currsor - curLine + 1;
+	char* CommandLine=new char[256];
+	char* command=new char[256];
+	char* shape=new char[256];
+	char* color = new char[256];
+	char* filename = new char[256];
+	int vertical=0, horizontal=0;
 	int startX = 0, startY = 0, endX = 0, endY = 0;
-	string line2;
-	string color;
-
-	input.seekg(0);
-
-	for (int n = 1; n < curLine; n += 1) // skipping the first lines before the opening tag
+	//string color;
+	while(1)
 	{
-		if (!getline(input, line2)) 
+		cout << "Enter command"<<endl;
+
+		cin.getline(command,100);
+
+		if (strcmp(command,"print")==0)
 		{
-			cout << "error";
+			array.PrintAll();
 		}
-	}
-	while (getline(input, line2))    //building the Container
-	{
-		if (LinesLeft==currsor)
+		else if (strcmp(command, "create")==0)
 		{
-			break;                   // I break before the closing </svg> tag
-		}
+			cout << "Enter shape:"<<endl;
+			cin.getline(shape, 100);
 
-		input.ignore(3);
-		getline(input, shape, ' ');
-		if (shape.compare("rect") == 0)
-		{
-			getline(input, line, '"');
-			input >> startX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> startY;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> endX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> endY;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			getline(input, color, '"');
-
-			int len = color.length() + 1;
-			char* ColorChar = new char[len];
-			for (int i = 0; i < len; i++) {
-				ColorChar[i] = color[i];
+			if (strcmp(shape, "rectangle") == 0)
+			{
+				cout << "Enter parameters:" << endl;
+				cin >> startX;
+				cin >> startY;
+				cin >> endX;
+				cin >> endY;
+				cin.ignore(1);
+				cin.getline(color, 100);
+				array.AddShape("rectangle", startX, startY, color, endX, endY);
 			}
-			array.AddShape("rectangle", startX, startY, ColorChar, endX, endY);
-			delete[] ColorChar;
-		}
-		else if(shape.compare("circle")==0)
-		{
-			getline(input, line, '"');
-			input >> startX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> startY;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> endX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			getline(input, color, '"');
-
-			int len = color.length() + 1;
-			char* ColorChar = new char[len];
-			for (int i = 0; i < len; i++) {
-				ColorChar[i] = color[i];
+			else if (strcmp(command,"circle") == 0 )
+			{
+				cout << "Enter parameters:" << endl;
+				cin >> startX;
+				cin >> startY;
+				cin >> endX;
+				cin.ignore(1);
+				cin.getline(color, 100);
+				array.AddShape("circle", startX, startY, color, endX);
 			}
-			array.AddShape("circle", startX, startY, ColorChar, endX, endY);
-			delete[] ColorChar;
-		}
-		else if (shape.compare("line"))
-		{
-			getline(input, line, '"');
-			input >> startX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> startY;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> endX;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			input >> endY;
-
-			getline(input, line, '=');
-			input.ignore(1);
-			getline(input, color, '"');
-
-			int len = color.length() + 1;
-			char* ColorChar = new char[len];
-			for (int i = 0; i < len; i++) {
-				ColorChar[i] = color[i];
+			else if (strcmp(command,"line") == 0)
+			{
+				cout << "Enter parameters:" << endl;
+				cin >> startX;
+				cin >> startY;
+				cin >> endX;
+				cin >> endY;
+				cin.ignore(1);
+				cin.getline(color, 100);
+				array.AddShape("line", startX, startY, color, endX, endY);
 			}
-			array.AddShape("line", startX, startY, ColorChar, endX, endY);
-			delete[] ColorChar;
+			else
+			{
+				cout << "Shape not supported.";
+			}
 		}
-		LinesLeft++;
-	}
+		else if (strcmp(command, "translate") == 0)
+		{
 
-	return array;
-}
-void WritingBackInSVGFile(const char* filename, ShapesContainer& array)// this function is building new svg file with changed information
-{
-	ofstream ofs;
-	ofs.open("temp.svg", ofstream::out);// using this file to build the whole file changing only the <svg> tagged section
-	int currsor = LinesCount(filename);//the total lines in the file
-	ifstream input;
-	string line;                     // I'm using that string to get rid of chars I do not need
-	string token = "<svg>";
-	input.open(filename);
-	unsigned int curLine = 0;
-	while (getline(input, line))     // curLine is the index of the start of the <svg> tag
-	{
-		curLine++;
-		if (line.find(token, 0) != string::npos)
+		}
+		else if (strcmp(command, "within") == 0)
+		{
+			cout << "Enter shape:" << endl;
+			cin.getline(shape, 100);
+			if (strcmp(shape, "rectangle") == 0)
+			{
+				cout << "Enter parameters:" << endl;
+				cin >> startX;
+				cin >> startY;
+				cin >> endX;
+				cin >> endY;
+				array.WithinRectangle(startX, startY, endX, endY);
+			}
+			else if (strcmp(shape, "circle") == 0)
+			{
+				cout << "Enter parameters:" << endl;
+				cin >> startX;
+				cin >> startY;
+				cin >> endX;
+				array.WithinCircle(startX, startY, endX);
+			}
+			else
+			{
+				cout << "Shape not supported.";
+			}
+		}
+		else if (strcmp(command, "open") == 0)
+		{
+			cout << "Enter file name:" << endl;
+			cin.getline(filename, 100);
+			input.open(filename);
+			array=ParseFile(filename);
+		}
+		else if (strcmp(command, "close") == 0)
+		{
+			input.close();
+		}
+		else if (strcmp(command,"save as")==0)
+		{
+			cout << "Enter svg file name:" << endl;
+			cin.getline(shape, 100);
+			WritingBackInSVGFile(shape, array);
+		}
+		else if (strcmp(command, "save") == 0)
+		{
+			WritingBackInSVGFile(filename, array);
+		}
+		else if (strcmp(command, "exit") == 0)
 		{
 			break;
 		}
-	}                                // I'm finding curLine by searching for the <svg> tag
-	input.seekg(0);
-	int LinesLeft = currsor - curLine + 1;
-	for (int i = 0; i < curLine; i++)
-	{
-		getline(input, line);
-		ofs << line << endl;
 	}
-	array.PrintAllInStrm(ofs);
-	ofs << "</svg>";
-
-	remove(filename);
-
-	rename("temp.svg", filename);
 }
+
