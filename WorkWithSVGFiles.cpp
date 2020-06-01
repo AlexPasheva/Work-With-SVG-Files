1d5b19d05500bf184c8f49c67951b5c98564dd53
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-06-01 23:29:22 +0300

    Update WorkWithSVGFiles.cpp

--- WorkWithSVGFiles.cpp
+++ WorkWithSVGFiles.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "SVGParser.h"
+int MAXLEN = 256;
 int main()
-{	ShapesContainer array;
+{
 	ifstream input;
-	ofstream ofs;
+	ShapesContainer array;
 	char* CommandLine=new char[MAXLEN];
 	char* command=new char[MAXLEN];
 	char* shape=new char[MAXLEN];
 	char* color = new char[MAXLEN];
 	char* filename = new char[MAXLEN];
-	int vertical=0, horizontal=0;
 	int startX = 0, startY = 0, endX = 0, endY = 0;
 
 	while(1)
@@ -65,8 +65,48 @@ int main()
 			}
 		}
 		else if (strcmp(command, "translate") == 0)
-		{
-
+		{//очаква команда във формат първо vertical=10 или 4 vertical=10 /нов ред/ horizontal=20
+			int n = 0;
+			int h = 0;
+			int v = 0;
+			char* horizontal = new char[30];
+			char* vertical = new char[30];
+			char* number = new char[15];
+			cin.getline(vertical, 30);
+			int i = 0;
+			while (vertical[i] >= 0 && vertical[i] <= 9)
+			{
+				number[i] = vertical[i];
+				i++;
+			}
+			n = atoi(number);
+			i = 0;
+			int j = 9;
+			while (vertical[j] >= 0 && vertical[j] <= 9)
+			{
+				number[i] = vertical[j];
+				i++;
+				j++;
+			}
+			v = atoi(number);
+			cin.getline(horizontal, 30);
+			i = 0;
+			j = 11;
+			while (horizontal[j] >= 0 && horizontal[j] <= 9)
+			{
+				number[i] = horizontal[j];
+				i++;
+				j++;
+			}
+			h = atoi(number);
+			if (n == 0)
+			{
+				array.TranslateShape(v, h);
+			}
+			array.TranslateShape(v, h, n);
+			delete[] vertical;
+			delete[] horizontal;
+			delete[] number;
 		}
 		else if (strcmp(command, "within") == 0)
 		{
@@ -99,6 +139,10 @@ int main()
 			cout << "Enter file name:" << endl;
 			cin.getline(filename, 100);
 			input.open(filename);
+			if (!input.is_open())
+			{
+				cout << "Couldn't open the file.";
+			}
 			array=ParseFile(filename);
 		}
 		else if (strcmp(command, "close") == 0)
@@ -128,5 +172,11 @@ int main()
 			Help();
 		}
 	}
+	delete[] CommandLine;
+	delete[] command;
+	delete[] shape;
+	delete[] color;
+	delete[] filename;
 }
+
 
